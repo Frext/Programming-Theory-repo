03fb4780d4a3a4304a3f17f1dd92839c1e6db4e8
Frext <furkanusul28@gmail.com>
2022-10-09 17:38:52 +0300

    Add player jump

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,12 +1,20 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
-    [Header("Movement")] 
+    [Header("Movement")]
     [SerializeField] private float moveSpeed;
     [SerializeField] private Transform orientation;
+
+    [Space] 
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float jumpCooldown;
+    [SerializeField] private float airMultiplier;
+
+    bool isReadyToJump = true;
 
     float horizontalInput;
     float verticalInput;
@@ -16,13 +24,13 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody playerRb;
 
 
-    [Header("Ground Check")]
+    [Header("Ground Check")] 
     [SerializeField] private float playerHeight;
+
     [SerializeField] private float groundDrag;
     [SerializeField] private LayerMask groundLayer;
-    
-    
-    bool isGrounded;
+
+    bool isPlayerGrounded;
 
     void Start()
     {
@@ -35,42 +43,53 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         GetInput();
+
+        // If this is called in FixedUpdate, the speed limit can still be exceeded.
+        ClampSpeed();
     }
 
-    void GetInput()
+    private void GetInput()
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetButton("Jump") && isReadyToJump && isPlayerGrounded)
+        {
+            isReadyToJump = false;
+
+            Jump();
+
+            // This lets you jump if you hold the jump button. 
+            StartCoroutine(IResetJumpState());
+        }
     }
 
     void FixedUpdate()
     {
         HandleDrag();
-        
+
         MovePlayer();
-        
-        ClampSpeed();
     }
 
     private void HandleDrag()
     {
-        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, groundLayer);
+        isPlayerGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.3f, groundLayer);
 
-        if (isGrounded)
-        {
-            playerRb.drag = groundDrag;
-        }
-        else
-        {
-            playerRb.drag = 0f;
-        }
+        playerRb.drag = isPlayerGrounded ? groundDrag : 0f;
     }
 
     private void MovePlayer()
     {
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
-        playerRb.AddForce(moveDirection.normalized * (moveSpeed * 10f), ForceMode.Force);
+        if (isPlayerGrounded)
+        {
+            playerRb.AddForce(moveDirection.normalized * (moveSpeed * 10f), ForceMode.Force);
+        }
+        else
+        {
+            playerRb.AddForce(moveDirection.normalized * (moveSpeed * 10f * airMultiplier), ForceMode.Force);
+        }
     }
 
     private void ClampSpeed()
@@ -84,5 +103,21 @@ public class PlayerMovement : MonoBehaviour
 
             playerRb.velocity = new Vector3(limitedVelocity.x, playerRb.velocity.y, limitedVelocity.z);
         }
+    }
+
+    private void Jump()
+    {
+        // Reset y velocity to jump the exact amount every time.
+        playerRb.velocity = new Vector3(playerRb.velocity.x, 0f, playerRb.velocity.z);
+
+        playerRb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+        print("Jump");
+    }
+
+    private IEnumerator IResetJumpState()
+    {
+        yield return new WaitForSeconds(jumpCooldown);
+
+        isReadyToJump = true;
     }
 }
