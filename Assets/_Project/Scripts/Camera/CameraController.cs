6bcf95a0477236e2e5a5044e83b5d0a484261339
Frext <furkanusul28@gmail.com>
2023-01-29 00:46:15 +0300

    Add a 3rd person camera
    - Included the Cinemachine asset in the project.
    
    - Recreated the 1st person camera and added a 3rd person camera.
    
    - Changed the camera controller to make it able to switch between cameras.
    
    - Added a cursor sprite.

--- Assets/_Project/Scripts/Camera/CameraController.cs
+++ Assets/_Project/Scripts/Camera/CameraController.cs
@@ -1,11 +1,49 @@
+using System;
+using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace _Project.Scripts.Camera
 {
     public class CameraController : MonoBehaviour
     {
-        [SerializeField] private float sensitivity;
+        [Header("Camera Properties")]
+        [SerializeField] private float mouseSensitivity;
+
+        [SerializeField] private List<GameObject> virtualCameraList;
         
+        int _currentCameraTransformIndex;
+        int CurrentCameraTransformIndex
+        {
+            get => _currentCameraTransformIndex;
+            set
+            {
+                // Rewind the index value to 0 if the set value equals to the count of the camera transforms.
+                if (value == virtualCameraList.Count)
+                {
+                    _currentCameraTransformIndex = 0;
+                }
+                else
+                {
+                    _currentCameraTransformIndex = Mathf.Clamp(value, 0, virtualCameraList.Count - 1);
+                }
+
+                UpdateCameraActiveStates();
+            }
+        }
+
+        private void UpdateCameraActiveStates()
+        {
+            for (int index = 0; index < virtualCameraList.Count; index++)
+            {
+                // If the current index is found, turn the camera object on.
+                virtualCameraList[index].SetActive(index == CurrentCameraTransformIndex);
+            }
+        }
+
+
+        [Header("Player Properties")]
         [Tooltip("This is the transform that camera will rotate around the y-axis.")]
         [SerializeField] private Transform orientation;
 
@@ -13,8 +51,11 @@ namespace _Project.Scripts.Camera
         float xRotation,
             yRotation;
 
+        
         void Start()
         {
+            CurrentCameraTransformIndex = 0;
+            
             LockCursor();
         }
 
@@ -24,6 +65,7 @@ namespace _Project.Scripts.Camera
             Cursor.visible = false;
         }
 
+        // This method is used when the game over UI is showed to activate the cursor.
         public void UnlockCursor()
         {
             Cursor.lockState = CursorLockMode.None;
@@ -32,16 +74,31 @@ namespace _Project.Scripts.Camera
 
         void Update()
         {
-            float mouseX = Input.GetAxisRaw("Mouse X") * sensitivity;
-            float mouseY = Input.GetAxisRaw("Mouse Y") * sensitivity;
+            HandleCameraSwitch();
+            
+            HandleMouseInput();
+        }
+        
+        private void HandleMouseInput()
+        {
+            float mouseX = Input.GetAxisRaw("Mouse X") * mouseSensitivity;
+            float mouseY = Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
 
             yRotation += mouseX;
 
             xRotation -= mouseY;
             xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-            
-            transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+
+            virtualCameraList[CurrentCameraTransformIndex].transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
             orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+        }
+        
+        private void HandleCameraSwitch()
+        {
+            if (Input.GetKeyDown(KeyCode.V))
+            {
+                CurrentCameraTransformIndex ++;
+            }
         }
 
         // This method is used by the player movement script.
